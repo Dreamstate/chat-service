fb46a9c575e3d4b6cb9150f6f086a77d5c93e65a
nick <nikolay.chobanov@x3men.net>
2024-09-08 19:06:16 +0300

    add logger and web server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,29 @@
-package cmd
+package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	app := fiber.New()
+
+	app.Get("/api/v1", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello from /api/v1",
+		})
+	})
+
+	port := ":8080"
+	log.Info().Msgf("Starting server on port %s...", port)
+	if err := app.Listen(port); err != nil {
+		log.Fatal().Err(err).Msg("Failed to start the server")
+	} else {
+		log.Info().Msg("Server started successfully")
+	}
+}
